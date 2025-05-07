1e654d3c974d76d2909fe330a8c5af7a22295d23
Jeff L <kitermerts@gmail.com>
2025-05-07 08:48:24 -0400

    updates.

--- app/components/WhatWeDo.tsx
+++ app/components/WhatWeDo.tsx
@@ -26,7 +26,7 @@ export default function WhatWeDo() {
         />
       </div>
 
-      {/* Blue Square Overlap */}
+      {/* Notch Thingy */}
       <div className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 w-36 h-36 bg-[#0078D7] z-20" />
 
 
