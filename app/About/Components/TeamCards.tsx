717612358b84f9693a8268d168e2c3ad401402c1
Jeff L <kitermerts@gmail.com>
2025-07-08 18:47:50 +0000

    updates.

--- app/About/Components/TeamCards.tsx
+++ app/About/Components/TeamCards.tsx
@@ -32,7 +32,7 @@ const team = [
   },
   {
     name: "Armaan R.",
-    title: "Director of Robotics",
+    title: "Founding Robotics Engineer",
     image: "/AR.jpeg",
     linkedin: "https://www.linkedin.com/in/armaanrasheed/",
     bio: "Designs and deploys robotics solutions for manufacturing and automation, working towards efficient, scalable production.",
