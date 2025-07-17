66cd847b84cb9a2d398c29a6cf10a01923099617
Jeff L <kitermerts@gmail.com>
2025-07-17 16:08:51 +0000

    changed globe

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
 import "./globals.css";
 import { Suspense } from "react";
 
-<link rel="icon" href="/favicon.ico" />
+
 
 export default function RootLayout({ children, }: { children: React.ReactNode; }) {
   return (
     <html lang="en" className="h-full overflow-hidden">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <Suspense>
         <body className="h-full bg-[#04012A]">
           {children}
