cef240420216e26861e8693227362f52018ae118
Jeff L <kitermerts@gmail.com>
2025-07-17 16:06:13 +0000

    changed globe

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import "./globals.css";
 import { Suspense } from "react";
 
+<link rel="icon" href="/favicon.ico" />
+
 export default function RootLayout({ children, }: { children: React.ReactNode; }) {
   return (
     <html lang="en" className="h-full overflow-hidden">
