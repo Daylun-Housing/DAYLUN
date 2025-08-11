ca2310f838105a08a17e52caceabf5268f7272e2
paliu8908 <paliu8908@gmail.com>
2025-08-11 11:45:28 -0400

    Updated the Article selection page and removed all old articles

---
+++ app/Articles/components/ArticleParts/content.tsx
@@ -0,0 +1,37 @@
+
+export interface art_button {
+    title: string;
+    src: string;
+    alt: string;
+    text: string;
+    goto: string;
+}
+
+export const contentData = [
+    {
+        title: "PRODUCT LAUNCH",
+        src: "/Bamboo.jpg",
+        alt: "Bamboo OSB",
+        text: `DAYLUN has launched its Bamboo OSB. A waterproof exterior protection
+            layer made from easy-to-work-with materials.`,
+        goto: "product_launch"
+    },
+
+    {
+        title: "241 WATERLOO",
+        src: "/flagship-house.jpg",
+        alt: "241 Waterloo House",
+        text: `241 Waterloo is a net-zero and modular home build incorporating 
+              revolutionary materials and technologies.`,
+        goto: "Waterloo_241"
+    },
+
+    {
+        title: "YUKON FACTORY",
+        src: "/how-we-do-it.jpg",
+        alt: "How We Do It",
+        text: `Our Yukon valley will be operation soon. This will be our first,
+            automated factory that will produce our flagship products.`,
+        goto: "Yukon_Factory"
+    }
+]
