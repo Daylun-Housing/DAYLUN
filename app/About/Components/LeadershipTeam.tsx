c0a0792e720ee25a34f43e153a019b45eea470cb
paliu8908 <paliu8908@gmail.com>
2025-08-13 10:15:50 -0400

    Summary
    Added new people to the leadership team.

--- app/About/Components/LeadershipTeam.tsx
+++ app/About/Components/LeadershipTeam.tsx
@@ -7,7 +7,52 @@ const team = [
     title: "Chief Executive Officer",
     image: "/about-the-designer-Arthur-Zankowicz.jpg",
     linkedin: "https://www.linkedin.com/in/arthur-z-a494a517b/",
-    bio: "Over 25 years in sustainable design across construction, fashion, sport, and film. Founder of Pain, Moreno, and 50 Unlimited Co.",
+    bio: `Over 25 years in sustainable design across construction, fashion, sport, and film. Founder of Pain, Moreno, and 50 Unlimited Co.
+          Arthur now turns his experience in sustainable fashion to construction, which drives Daylun's pioneering approach in construction.`,
+  },
+  {
+    name: "Janey Shin",
+    title: "Co-Founder",
+    image: "/janeyshin.jpg",
+    linkedin: "https://www.linkedin.com/in/janeyshin",
+    bio: `Co-Founder of Daylun, with 20+ years of experience in healthcare and life sciences data, driving innovation through analytics. She has
+          led multinational pharma companies in pioneering approaches and influenced government policy.`,
+  },
+  {
+    name: "Elmu Hernandez",
+    title: "Bim Modelling",
+    image: "/placeholder.png",
+    bio: `With over 25+ years of experience in 3D modelling and Rivet, Elmu has delivered high-quality solutions across the globe, including in
+          Europe, Asia, and North America. He now contributes his innovative and precise engineer to Daylun.`,
+  },
+  {
+    name: "Dr. Beck Lee",
+    title: "Chemical Engineer",
+    image: "/placeholder.png",
+    bio: `Dr. Lee is a leading expert in chemistry and green product innovation, specializing in sustainability. He has collaborated with top sports
+        brands to develop eco-friendly solutions and is a professor at Xiamen University.`,
+  },
+  {
+    name: "Chris St. Jacques",
+    title: "Project Manager",
+    image: "/placeholder.png",
+    bio: `Chris brings 15 years of experience in project and supply chain management, with a strong focus on efficiency and execution. Based in Southeast
+          Asia, he oversees Daylun's international projects with precision and dedication. `,
+  },
+  {
+    name: "Karl Bruce",
+    title: "Sales Manager",
+    image: "/karlbruce.jpg",
+    linkedin: "https://www.linkedin.com/in/connectwithkarlbruce",
+    bio: `With 15+ years of experience in the Canadian Construction landscape, our Sales Manager has a deep understanding of the industry. He leverages this
+          understanding to drive growth and forge strong client relationships.`,
+  },
+  {
+    name: "Peter Rupert",
+    title: "Chief Technology Officer",
+    image: "/placeholder.png",
+    bio: `An Aeronautical and Mechanical Engineer specializing in Computer-Aided Engineering who has collaborated with Arthur Zankowicz in materials
+          development, his knowledge of materials is invaluable in research and development, as well as in using AI and automation to optimize factory operations.`,
   }
 ];
 
@@ -17,7 +62,7 @@ export default function LeaderCard() {
       <h2 className="text-4xl font-extrabold mb-10 text-center">
         Our Leadership
       </h2>
-      <div className="grid gap-10">
+      <div className="grid grid-cols-3 gap-10">
         {team.map((person, idx) => (
           <div
             key={idx}
@@ -34,14 +79,14 @@ export default function LeaderCard() {
             <h3 className="text-xl font-bold">{person.name}</h3>
             <p className="text-sm text-[#87a0b4]">{person.title}</p>
             <p className="text-sm mt-4">{person.bio}</p>
-            <a
+            {(person.linkedin && <a
               href={person.linkedin}
               target="_blank"
               rel="noopener noreferrer"
               className="mt-4 text-[#0474BC] hover:text-[#0f99e0]"
             >
               <FaLinkedinIn size={20} />
-            </a>
+            </a>)}
           </div>
         ))}
       </div>
