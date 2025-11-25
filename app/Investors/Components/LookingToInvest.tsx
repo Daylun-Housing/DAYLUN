3a9c406845d81a3b6d46fbbf9e012ae530cac7b3
Jeff <kitermerts@gmail.com>
2025-11-25 11:50:45 -0500

    updates.

--- app/Investors/Components/LookingToInvest.tsx
+++ app/Investors/Components/LookingToInvest.tsx
@@ -95,7 +95,7 @@ export default function LookingToInvest() {
             
             {/* PDF Viewer */}
             <iframe
-              src="Daylun_Micro_factory_Pitch_Deck.pdf"
+              src="DaylunConstruction&Materials.pdf"
               className="w-full h-full"
               title="Daylun Pitch Deck"
             />
